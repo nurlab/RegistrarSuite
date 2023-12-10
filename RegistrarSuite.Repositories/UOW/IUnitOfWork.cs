7f553212a68e791ec734203bd1404ab9d2035086
MohammedNuru <mohammed.nuru@teknikacorp.com>
2023-12-10 19:23:13 +0300

    feat: Initialize .NET Core project with basic architecture
    Created the initial structure of the .NET Core application, including:
    
    - Set up project files and directories
    - Implemented a basic layered architecture (Presentation, Business Logic, Data Access)
    - Added necessary configuration files
    - Included a default .gitignore file for Visual Studio and Rider
    
    This lays the foundation for the application, providing a starting point for further development and feature additions.

---
+++ RegistrarSuite.Repositories/UOW/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace RegistrarSuite.Repositories.UOW
+{
+    public interface IUnitOfWork<T>
+    {
+        int Commit();
+        Task<int> CommitAsync();
+    }
+}
