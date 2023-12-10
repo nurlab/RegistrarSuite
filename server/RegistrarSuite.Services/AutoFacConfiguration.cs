16a4b15d19313016007890396bb91db770938348
MohammedNuru <mohammed.nuru@teknikacorp.com>
2023-12-10 22:23:26 +0300

    feat: configure Autofac dependencies and AutoMapper profiles
    - Added Autofac configuration in 'AutoFacConfiguration.cs' to register dependencies:
      - Registered unit of work (UnitOfWork) and associated repositories for entities: Student, FamilyMember, and Country
      - Registered corresponding services: StudentService, FamilyMemberService, and CountryService
    
    - Implemented AutoMapper profiles in 'MappingProfile.cs':
      - Created mappings between entity models and DTOs for Student, FamilyMember, and Country entities
      - Added mappings for basic and detailed representations
    
    These changes enhance dependency injection and facilitate object-to-object mapping for improved service functionality.

---
+++ server/RegistrarSuite.Services/AutoFacConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using RegistrarSuite.Repositories.Metadata;
+using RegistrarSuite.Repositories.UOW;
+using RegistrarSuite.Services.Metadata;
+using RegistrarSuite.Services.Students;
+
+namespace RegistrarSuite.Services
+{
+    public class AutoFacConfiguration : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+            // Register unit of work
+            builder.RegisterGeneric(typeof(UnitOfWork<>)).As(typeof(IUnitOfWork<>)).InstancePerDependency();
+
+            builder.RegisterType<StudentService>().As<IStudentService>().InstancePerDependency();
+            builder.RegisterType<StudentRepository>().As<IStudentRepository>().InstancePerDependency();
+
+            builder.RegisterType<FamilyMemberService>().As<IFamilyMemberService>().InstancePerDependency();
+            builder.RegisterType<FamilyMemberRepository>().As<IFamilyMemberRepository>().InstancePerDependency();
+
+            builder.RegisterType<CountryService>().As<ICountryService>().InstancePerDependency();
+            builder.RegisterType<CountryRepository>().As<ICountryRepository>().InstancePerDependency();
+
+        }
+    }
+}
---
+++ server/RegistrarSuite.Services/MappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using AutoMapper;
+using RegistrarSuite.Data.Models.MetadataSchema;
+using RegistrarSuite.Data.Models.StudentSchema;
+using RegistrarSuite.DTO.Metadata;
+using RegistrarSuite.DTO.Students;
+
+namespace RegistrarSuite.Services
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Student, StudentBasicDto>().ReverseMap();
+            CreateMap<Student, StudentNationalityDto>()
+                .ForMember(dest => dest.NationalityId, opt => opt.MapFrom(src => src.Nationality == null ? 0 : src.Nationality.Id)).ReverseMap();
+            CreateMap<FamilyMember, FamilyMemberDto>().ReverseMap();
+            CreateMap<FamilyMember, FamilyMemberBasicDto>().ReverseMap();
+            CreateMap<FamilyMember, FamilyMemberBasicResponseDto>().ReverseMap();
+
+            CreateMap<Country, CountryDto>().ReverseMap();
+
+        }
+
+
+    }
+
+
+}
