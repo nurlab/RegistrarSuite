1e8e340e7fc7ea35e31f772d86ba296ac0f8e9a4
MohammedNuru <mohammed.nuru@teknikacorp.com>
2023-12-10 22:17:00 +0300

    feat(data): add BaseEntity and AppDbContext
    - Implemented BaseEntity class with Id, IsDeleted, IsActive, CreatedBy, CreatedOn, UpdatedBy, and UpdatedOn properties, representing a common entity structure
    - Created AppDbContext class as the database context for the application, inheriting from DbContext
    - Added DbSet properties for Student, FamilyMember, and Country models in the AppDbContext
    - Configured AppDbContext with DbContextOptions in the constructor

---
+++ server/RegistrarSuite.Data/BaseModeling/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using RegistrarSuite.Core.Interfaces;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistrarSuite.Data.BaseModeling
+{
+    public class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+
+    }
+}
---
+++ server/RegistrarSuite.Data/DataContext/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrarSuite.Data.Models.MetadataSchema;
+using RegistrarSuite.Data.Models.StudentSchema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrarSuite.Data.DataContext
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            //Database.EnsureCreated();
+            //Database.Migrate();
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<FamilyMember> FamilyMembers { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+    }
+}
